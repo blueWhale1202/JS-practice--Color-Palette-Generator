6ae31dda63cf94811489267436a95e0be424f0c0
mrfour4 <21521609@gm.uit.edu.vn>
2023-02-23 10:34:04 +0700

    :active replace for mouse event

--- main.js
+++ main.js
@@ -1,57 +1,51 @@
 const NUMBER_OF_PALETTES = 32;
 
-const generateHexCode = () =>
-    Math.floor(Math.random() * 0xffffff)
+const refreshBtn = document.querySelector(".refresh-btn");
+const palettesList = document.querySelector(".palette-list");
+
+const generateHexCode = () => {
+    const hexCode = Math.floor(Math.random() * 0xffffff)
         .toString(16)
-        .padStart(6, "0")
-        .toLocaleUpperCase();
+        .padStart(6, "0");
+    return `#${hexCode}`;
+};
 
-const paletteList = document.querySelector(".palette-list");
-const generatePalettes = () => {
-    paletteList.innerHTML = "";
+const copyColor = async (paletteItem, hexCode) => {
+    try {
+        const paletteInfo = paletteItem.querySelector(".palette__info");
+        paletteInfo.innerText = "Copied";
+
+        setTimeout(() => (paletteInfo.innerText = hexCode), 1000);
+    } catch (error) {
+        alert("Failed to copy the color code");
+    }
+};
+
+const generatePalette = () => {
+    palettesList.innerHTML = "";
+
     for (let i = 0; i < NUMBER_OF_PALETTES; i++) {
-        const hexCode = `#${generateHexCode()}`;
-
         const paletteItem = document.createElement("li");
         paletteItem.classList.add("palette-item");
+
+        const hexCode = generateHexCode();
 
         paletteItem.innerHTML = `
             <div
                 class="palette__color"
                 style="background-color: ${hexCode}"
             ></div>
-            <div class="palette__info">${hexCode}</div>
+            <p class="palette__info">${hexCode}</p>
         `;
 
-        paletteItem.onmousedown = () => {
-            paletteItem.style.transform = `scale(0.98)`;
-        };
+        paletteItem.onclick = (e) => copyColor(paletteItem, hexCode);
 
-        paletteItem.onmouseup = () => {
-            paletteItem.style.transform = `scale(1)`;
-        };
-
-        paletteItem.onclick = async () => {
-            try {
-                await navigator.clipboard.writeText(hexCode);
-
-                const paletteInfo = paletteItem.querySelector(".palette__info");
-                paletteInfo.innerText = "COPIED";
-
-                setTimeout(() => {
-                    paletteInfo.innerText = `${hexCode}`;
-                }, 600);
-            } catch (error) {
-                throw Error("Failed to copy: ", error);
-            }
-        };
-        paletteList.append(paletteItem);
+        palettesList.appendChild(paletteItem);
     }
 };
 
-generatePalettes();
+generatePalette();
 
-const refreshBtn = document.querySelector(".btn");
 refreshBtn.onclick = () => {
-    generatePalettes();
+    generatePalette();
 };
